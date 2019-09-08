9532f7f3bca729fe5927e3c7c4b4561d8baec068
Marbella <joo.luiz.98@outlook.com>
2019-09-07 23:15:16 -0300

    Unit tests to TipoDenuncia, Create User and All_denuncias

--- rotasegura/accounts/tests.py
+++ rotasegura/accounts/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+class AccountTests(APITestCase):
+    def test_create_user(self):
+        client = APIClient()
+        userData = {'username': 'testes', 'password': '123', 'email': 'testes@email.com'}
+        request = client.post('/api/auth/register', userData, format='json')
+        
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testes')
--- rotasegura/all_denuncias/tests.py
+++ rotasegura/all_denuncias/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from tipo_denuncia.models import TipoDenuncia
 
-# Create your tests here.
+class TipoDenunciaTests(APITestCase):
+    # Success Tests
+
+    def test_create_tipoDenuncia_done(self):
+        client = APIClient()
+
+        request = client.get('/api/all_denuncias/')
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
